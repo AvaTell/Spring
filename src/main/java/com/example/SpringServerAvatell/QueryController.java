6ce76fa2859dc3ec297bcc17d5408163fc4b88f0
Amy Cohen <anbcohen@gmail.com>
2018-09-11 11:07:55 -0700

    added additional query to create a transaction but it is currently non-functional and gives back a 400 error. Minor text changes to html

--- src/main/java/com/example/SpringServerAvatell/QueryController.java
+++ src/main/java/com/example/SpringServerAvatell/QueryController.java
@@ -8,6 +8,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -32,8 +33,8 @@ import java.util.Base64;
 @SessionAttributes({"username","password"})
 @ResponseBody()
 public class QueryController {
-    @GetMapping("/query")
-    public String queryPage(HttpServletRequest request, Model model, @RequestParam String country, @RequestParam String zipCode){
+    @GetMapping("/query/byzipcode")
+    public String queryByZipCode(HttpServletRequest request, Model model, @RequestParam String country, @RequestParam String zipCode){
         String result = null;
 
         if(model.asMap().get("username")==null||model.asMap().get("password")==null){
@@ -48,7 +49,7 @@ public class QueryController {
         // "Authorization: Basic Y29oZW5hbXl2QGdtYWlsLmNvbTpCaWdyZWRmaXNoNzIh" -H
         try{
             AvaTaxClient client = new AvaTaxClient("Test","1.0","localhost",AvaTaxEnvironment.Production).withSecurity(user,pass);
-            URL requestURL = new URL("https://rest.avataxcom/api/v2/taxrates/bypostalcode?country="+country+"&postalCode="+zipCode);
+//            URL requestURL = new URL("https://rest.avataxcom/api/v2/taxrates/bypostalcode?country="+country+"&postalCode="+zipCode);
 //
 //            String authorized = Base64.getEncoder().encodeToString((user+":"+pass).getBytes());
 //
@@ -72,7 +73,9 @@ public class QueryController {
             String authorized = Base64.getEncoder().encodeToString((user+":"+pass).getBytes());
             String requestURL = "https://rest.avatax.com/api/v2/taxrates/bypostalcode?country="+country+"&postalCode="+zipCode;
 
-
+/*
+RESOURCES: https://hc.apache.org/httpcomponents-client-ga/quickstart.html
+ */
             CloseableHttpClient httpclient = HttpClients.createDefault();
 //            HttpGet getRequest = new HttpGet(
 //                    "https://rest.avatax.com/api/v2/taxratesbyzipcode/download/2018-09-10");
@@ -95,6 +98,67 @@ public class QueryController {
             while ((output = br.readLine()) != null) {
                 if (output.contains(zipCode))
                 System.out.println(output);
+                result = output;
+            }
+
+            return result;
+        } catch (ClientProtocolException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+
+        return result;
+    }
+
+
+    @PostMapping("/query/bytaxcode")
+    public String queryByTaxCode(HttpServletRequest request, Model model, @RequestParam String taxcode, @RequestParam String description, @RequestParam String taxzipcode){
+        String result = null;
+
+        if(model.asMap().get("username")==null||model.asMap().get("password")==null){
+            return"redirect:/index";
+        }
+        String user=model.asMap().get("username").toString();
+        String pass=model.asMap().get("password").toString();
+
+        RestTemplate rTemp = new RestTemplate();
+
+        try {
+
+            String authorized = Base64.getEncoder().encodeToString((user+":"+pass).getBytes());
+            String requestURL = "https://rest.avatax.com/api/v2/transactions/create";
+
+/*
+RESOURCES: https://hc.apache.org/httpcomponents-client-ga/quickstart.html
+ */
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+//            HttpGet getRequest = new HttpGet(
+//                    "https://rest.avatax.com/api/v2/taxratesbyzipcode/download/2018-09-10");
+            HttpPost postRequest = new HttpPost(requestURL);
+            postRequest.addHeader("accept", "application/json");
+            postRequest.addHeader("authorization", "Basic " + authorized);
+            postRequest.addHeader("-d ", "\"{ \\\"lines\\\": [ { \\\"taxCode\\\": \\\"PS081282\\\", \\\"description\\\": \\\"Yarn\\\" } ], \\\"type\\\": \\\"SalesOrder\\\", \\\"companyCode\\\": \\\"DEFAULT\\\", \\\"date\\\": \\\"2018-09-11\\\", \\\"customerCode\\\": \\\"ABC\\\", \\\"purchaseOrderNo\\\": \\\"2018-09-11-001\\\", \\\"addresses\\\": { \\\"singleLocation\\\": { \\\"line1\\\": \\\"110 180th Street Court East\\\", \\\"city\\\": \\\"Spanaway\\\", \\\"region\\\": \\\"WA\\\", \\\"country\\\": \\\"US\\\", \\\"postalCode\\\": \\\"98387\\\" } }, \\\"commit\\\": false, \\\"currencyCode\\\": \\\"USD\\\",}");
+
+            HttpResponse response = httpclient.execute(postRequest);
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                if (output.contains(taxzipcode))
+                    System.out.println(output);
                 result = output;
             }
 
